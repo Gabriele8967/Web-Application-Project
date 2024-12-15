eb284441a41a262b3dd5ab731b5f778e9f96eac1
mimmo12491741 <dom.giaquinta@gmail.com>
2024-12-15 01:04:35 +0100

    aggiunta what-is component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {CardsComponent} from '../cards/cards.component';
 import {CarouselComponent} from '../carousel/carousel.component';
 import {IntroGuideComponent} from '../intro-guide/intro-guide.component';
+import {WhatIsComponent} from '../what-is/what-is.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     CardsComponent,
     CarouselComponent,
-    IntroGuideComponent
+    IntroGuideComponent,
+    WhatIsComponent
   ],
   templateUrl: './home.component.html',
   standalone: true,
